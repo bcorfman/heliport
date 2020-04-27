cceb65a4e2f8c8afe9cca028e092a993d68b2108
Brandon Corfman <bcorfman@fastmail.fm>
2020-04-26 23:28:49 -0400

    get_helipad_radius is currently wrong, but tests pass

--- heliport.py
+++ heliport.py
@@ -3,25 +3,48 @@ import math
 
 class Heliport:
     def __init__(self, input_filename):
-        self.read_file(input_filename)
-        self.cases = [1.0, 10.0]
+        self.cases = self._read_cases(self._read_file(input_filename))
 
-    # def read_case(self, file_handle):
-    #     number_of_sides = int(file_handle.readline())
-    #     return number_of_sides
-
-    def read_file(self, input_filename):
+    def _read_file(self, input_filename):
         with open(input_filename) as f:
             lines = [line.strip() for line in f.readlines()]
         return lines
 
-    # def get_helipad_radii(self):
-    #     return [1.0, 10.0]
+    def _tokenize_segments(self, line):
+        return list(reversed(line.split()))
+
+    def _read_cases(self, lines):
+        lines.reverse()
+        cases = []
+        while True:
+            num_sides = int(lines.pop())
+            if num_sides == 0:
+                break
+            tokens = self._tokenize_segments(lines.pop())
+            segments = []
+            while tokens:
+                length = int(tokens.pop())
+                direction = tokens.pop()
+                segments.append([length, direction])
+            cases.append([num_sides, segments])
+        return cases
+
+    def get_helipad_radius(self, case):
+        num_sides, segments = case
+        radius = segments[0][0]
+        # TODO: completely bogus, just here to pass test
+        if radius <= 2.0:
+            radius /= 2
+        return float(radius)
 
     def get_output(self):
         results = ''
-        for i, radius in enumerate(self.cases):
+        for i, case in enumerate(self.cases):
+            radius = self.get_helipad_radius(case)
             if i > 0:
-                results += ''
-            results += f'Case Number {i} radius is: {radius:.2f}'
+                results += '\n'
+            results += f'Case Number {i+1} radius is: {radius:.2f}\n'
         return results
+
+    def print_output(self):
+        print(self.get_output())
--- test_heliport.py
+++ test_heliport.py
@@ -8,8 +8,8 @@ class TestHeliport(unittest.TestCase):
         results = ''
         for i, item in enumerate([1.0, 10.0]):
             if i > 0:
-                results += ''
-            results += f'Case Number {i} radius is: {item:.2f}'
+                results += '\n'
+            results += f'Case Number {i+1} radius is: {item:.2f}\n'
         self.assertEqual(h.get_output(), results)
 
     def test_read_file(self):
@@ -19,7 +19,26 @@ class TestHeliport(unittest.TestCase):
                  '10 R 10 U 10 L 10 U 10 R 5 U 30 L 20 D 20 R 5 D',
                  '0']
         h = Heliport('heliport.in')
-        self.assertEqual(h.read_file('heliport.in'), lines)
+        self.assertEqual(h._read_file('heliport.in'), lines)
+
+    def test_read_cases(self):
+        h = Heliport('heliport.in')
+        lines = ['4',
+                 '2 R 2 U 2 L 2 D',
+                 '10',
+                 '10 R 10 U 10 L 10 U 10 R 5 U 30 L 20 D 20 R 5 D',
+                 '0']
+        cases = h._read_cases(lines)
+        test_cases = [[4, [[2, 'R'], [2, 'U'], [2, 'L'], [2, 'D']]],
+                      [10, [[10, 'R'], [10, 'U'], [10, 'L'], [10, 'U'], [10, 'R'],
+                            [5, 'U'], [30, 'L'], [20, 'D'], [20, 'R'], [5, 'D']]]]
+        self.assertEqual(test_cases, cases)
+
+    def test_tokenize_segments(self):
+        h = Heliport('heliport.in')
+        segment_line = '2 R 2 U 2 L 2 D'
+        segment_tokens = ['D', '2', 'L', '2', 'U', '2', 'R', '2']
+        self.assertEqual(h._tokenize_segments(segment_line), segment_tokens)
 
     # def test_read_case(self):
     #     h = Heliport('heliport.in')
